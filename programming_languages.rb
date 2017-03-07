b165b693f9707510881892414a356eadc9c78684
Meryl Dakin <meryl.k.dakin@gmail.com>
2017-03-07 16:53:39 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_format = {}
+  languages.keys.each do |types|
+    languages[types].each do |language, data|
+      new_format[language] = data
+      new_format[language][:style] = []
+    end
+  end
+  new_format.each do |language, data_hash|
+    if languages[:oo].include?(language)
+      data_hash[:style].push(:oo)
+    end
+    if languages[:functional].include?(language)
+      data_hash[:style].push(:functional)
+    end
+  end
+  return new_format
 end
